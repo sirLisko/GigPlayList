6ce6d96e16b48e49402cee3d689c274e2bef8e37
Luca Lischetti <sirlisko@gmail.com>
2015-11-11 07:13:14 +0000

    modularised spotify and extra calls now are in parallel

--- modules/spotify.js
+++ modules/spotify.js
@@ -1,18 +1,20 @@
 'use strict';
 
 var _ = require('lodash');
+var request = require('request');
 
-function parser(body){
-	return _
-		.chain(JSON.parse(body).tracks.items)
-		.map(function (track) {
+var spotifyUrl = 'https://api.spotify.com/v1/search?q={{artist}}&type=track&limit=50';
+
+function parser(artist, cb){
+	request(spotifyUrl.replace('{{artist}}', artist), function(err, resp, body){
+		cb(err, _.map(JSON.parse(body).tracks.items, function (track) {
 			return {
 				artist: track.artists[0].name,
 				title: track.name.toLowerCase(),
 				link: track.uri
 			};
-		})
-		.value();
+		}));
+	});
 }
 
 module.exports = parser;
--- routes.js
+++ routes.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var request = require('request');
 var express = require('express');
+var async = require('async');
+
+var setList = require('setlistfm-parser');
 var spotifyParser = require('./modules/spotify');
 var trackMerger = require('./modules/merger');
 var songkick = require('./modules/songkick');
+
 var router = express.Router();
-
-var setList = require('setlistfm-parser');
-
-var url = 'https://api.spotify.com/v1/search?q={{artist}}&type=track&limit=50';
 
 router.get('/', function(req, res) {
 	res.render('index');
@@ -20,16 +19,18 @@ router.get('/:artist', function(req, res) {
 	var clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
 	setList.getTracks(artist).then(function(setList){
-		request(url.replace('{{artist}}', artist), function(err, resp, body){
-			trackMerger(setList, spotifyParser(body));
-
-			songkick(artist, clientIp, function(events){
-				setList.events = events;
-				res.render('result', setList);
-			});
-
+		async.parallel({
+			spotify: function(cb){
+				spotifyParser(artist, cb);
+			},
+			songkick: function(cb){
+				songkick(artist, clientIp, cb);
+			}
+		}, function(err, results) {
+			trackMerger(setList, results.spotify);
+			setList.events = results.songkick;
+			res.render('result', setList);
 		});
-
 	}).catch(function (error) {
 		res.render('result', {
 			artist: artist,
