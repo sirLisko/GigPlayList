cd3d0d581454903811ebf4403039a5dacfec74d1
Luca Lischetti <luca@sirlisko.com>
2022-05-16 21:38:00 +0100

    feat: added second batch of spotify songs

--- utils/apis/spotify.ts
+++ utils/apis/spotify.ts
@@ -7,16 +7,26 @@ var spotifyApi = new SpotifyWebApi({
   clientSecret: SPOTIFY_SECRET,
 });
 
-export const getArtistTracks = async (artist_name: string) => {
-  const {
-    body: { access_token },
-  } = await spotifyApi.clientCredentialsGrant();
-  spotifyApi.setAccessToken(access_token);
+const getSongs = async (artist_name: string, offset: number = 0) => {
   const { body } = await spotifyApi.searchTracks(`artist:${artist_name}`, {
     limit: 50,
+    offset,
   });
   return body?.tracks?.items?.map((track) => ({
     title: track.name.toLowerCase(),
     uri: track.uri,
   }));
+};
+
+export const getArtistTracks = async (artist_name: string) => {
+  const {
+    body: { access_token },
+  } = await spotifyApi.clientCredentialsGrant();
+  spotifyApi.setAccessToken(access_token);
+
+  const batch = await Promise.all([
+    getSongs(artist_name, 0),
+    getSongs(artist_name, 50),
+  ]);
+  return [...(batch?.[0] || []), ...(batch?.[1] || [])];
 };
