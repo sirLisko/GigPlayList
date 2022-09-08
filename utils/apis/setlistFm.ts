b4c0d909e7f395b77c6ada77fa2d11aae3ce9d52
Luca Lischetti <luca@sirlisko.com>
2022-09-08 11:04:11 +0100

    feat: improved search for artist (#16)

--- utils/apis/setlistFm.ts
+++ utils/apis/setlistFm.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const DOMAIN = "https://api.setlist.fm";
-const PATH = "/rest/1.0/search/setlists?artistName=";
+const ARTIST_PATH = "/rest/1.0/search/artists?sort=relevance&artistName=";
+const SETLIST_PATH = "/rest/1.0/search/setlists?artistMbid=";
 
 const headers = {
   Accept: "application/json",
@@ -9,8 +10,14 @@ const headers = {
 };
 
 export const getArtistSetlist = async (artist: string) => {
-  const { data } = await axios(`${DOMAIN}${PATH}${artist}`, {
+  const { data: artistData } = await axios(`${DOMAIN}${ARTIST_PATH}${artist}`, {
     headers,
   });
+  const { data } = await axios(
+    `${DOMAIN}${SETLIST_PATH}${artistData?.artist?.[0].mbid}`,
+    {
+      headers,
+    }
+  );
   return data;
 };
