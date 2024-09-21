84fed130b83b9043fec7011090a8dbd9cc0dab43
Luca Lischetti <luca@sirlisko.com>
2024-09-21 09:32:13 +0100

    chore/removed not necessary dependencies (#24)
    * chore: removed unused fonts
    
    * chore: removed unnecessary package

--- pages/api/artists/[artistName]/spotify.ts
+++ pages/api/artists/[artistName]/spotify.ts
@@ -1,4 +1,4 @@
-import { StatusCodes } from "http-status-codes";
+import { HttpStatusCode } from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getArtistTracks } from "server/apis/spotify";
@@ -6,18 +6,18 @@ import { getArtistTracks } from "server/apis/spotify";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { artistName } = req.query as { artistName: string };
   if (!artistName) {
-    res.status(StatusCodes.BAD_REQUEST).end();
+    res.status(HttpStatusCode.BadRequest).end();
   }
   try {
     const artistData = await getArtistTracks(artistName);
-    res.status(StatusCodes.OK).json(artistData);
+    res.status(HttpStatusCode.Ok).json(artistData);
   } catch (e: any) {
     console.error(e);
     res
       .status(
         e?.response?.data?.code ??
           e?.status ??
-          StatusCodes.INTERNAL_SERVER_ERROR,
+          HttpStatusCode.InternalServerError,
       )
       .end(
         e?.response?.data?.message ?? e?.message ?? "Ops! There was a problem!",
--- pages/api/artists/[artistName]/tracks.ts
+++ pages/api/artists/[artistName]/tracks.ts
@@ -1,21 +1,20 @@
-import { StatusCodes } from "http-status-codes";
-
 import { getArtistSetlist } from "server/apis/setlistFm";
 import { getAggregatedSetlists } from "server/setlists";
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import { HttpStatusCode } from "axios";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { artistName } = req.query as { artistName: string };
   if (!artistName) {
-    return res.status(StatusCodes.BAD_REQUEST).end();
+    return res.status(HttpStatusCode.BadRequest).end();
   }
   try {
     const setList = await getArtistSetlist(artistName);
-    res.status(StatusCodes.OK).json(getAggregatedSetlists(setList));
+    res.status(HttpStatusCode.Ok).json(getAggregatedSetlists(setList));
   } catch (e: any) {
     res
-      .status(e?.response?.data?.code ?? StatusCodes.INTERNAL_SERVER_ERROR)
+      .status(e?.response?.data?.code ?? HttpStatusCode.InternalServerError)
       .end(e?.response?.data?.message || "Ops! There was a problem!");
   }
 };
