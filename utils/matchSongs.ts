d2260feb9b549e28de967558b44c362590b31b72
Luca Lischetti <luca@sirlisko.com>
2024-10-07 01:56:36 +0200

    refactor: improved match

--- utils/matchSongs.ts
+++ utils/matchSongs.ts
@@ -1,13 +1,23 @@
 import { Link, Track } from "types";
 
+export const normalizeSong = (str: string) =>
+  str
+    .normalize("NFD") // Decompose accents
+    .replace(/[\u0300-\u036f]/g, "") // Remove accents
+    .replace(/[^a-zA-Z0-9\s]/g, "") // Remove special characters
+    .toLowerCase(); // Convert to lowercase for case-insensitive comparison
+
 export const isSameSong = (linkTitle: string, title: string) => {
-  const song = linkTitle.toLowerCase();
-  return song === title || song.split(" (feat. ")?.[0] === title;
+  const song = normalizeSong(linkTitle);
+  const normalizedTitle = normalizeSong(title);
+  return (
+    song === normalizedTitle ||
+    normalizeSong(linkTitle.split(" - ")?.[0]) === normalizedTitle ||
+    normalizeSong(linkTitle.split(" (feat. ")?.[0]) === normalizedTitle
+  );
 };
 
-export const matchSongs = (tracks: Track[], links: Link[]) =>
+export const matchSongs = (tracks: Track[], links: Link[]): Link[] =>
   tracks
-    .map(
-      ({ title }) => links?.find((link) => isSameSong(link.title, title))?.uri,
-    )
-    .filter(Boolean) as string[];
+    .map(({ title }) => links.find((link) => isSameSong(link.title, title)))
+    .filter((link) => Boolean(link?.uri)) as Link[];
